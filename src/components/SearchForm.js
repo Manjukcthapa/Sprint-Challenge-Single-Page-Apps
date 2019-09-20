03501285aac09bd986f11b6d978003aab9c957e1
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 13:54:05 -0400

    changed the styleing of searchForm component

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import Styled from "styled-components";
 
+const Input = Styled.input`
+margin-left:400px;
+height:40px;
+width:250px;
+font-size:20px;
+
+`
 export default function SearchForm(props) {
  
   return (
     <section className="search-form">
-   <input
+   <Input
         id="name"
         type="text"
         name="textfield"
