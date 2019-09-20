f751c0cfaf3e135b0022cf60e64191bfcd35e94c
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 14:09:27 -0400

    used axios and .get to get api data

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Styled from "styled-components";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+
+
+
+export default function CharacterCard(props) {
+  return (
+    <div>
+      <div>
+        <p>
+          <Img src={props.char.image}></Img>
+        </p>
+        <div>
+          <P>{props.char.name}</P>
+          <P>{props.char.type}</P>
+          <P>{props.char.dimension}</P>
+          
+        </div>
+      
+      </div>
+    </div>
+  );
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,25 @@
 import React, { useEffect, useState } from "react";
+import CharacterCard from "../components/LocationCard";
+export default function LocationsList() {
+  const [characters, setCharacters] = useState([]);
 
-export default function LocationsList() {}
+  useEffect(() => {
+    axios
+      .get(` https://rickandmortyapi.com/api/character/`)
+      .then(res => {
+        setCharacters(res.data.results);
+        console.log("results", res.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {characters.map(item => {
+        return <CharacterCard key={item.id} char={item} />;
+      })}
+    </div>
+  );
+}
