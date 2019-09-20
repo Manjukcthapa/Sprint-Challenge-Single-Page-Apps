d07427de0ec417c7876bcfdbc8bbcc52093513b0
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 14:12:03 -0400

    import locationList to the router

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import WelComePage from "./WelcomePage";
 import CharacterList from "./CharacterList";
+import LocationList from "./LocationsList"
 
 export default function Router() {
   
@@ -10,6 +11,7 @@ export default function Router() {
     <div className="page-view ui botton segment active tab">
       <Switch>
         <Route path="/characters" component={CharacterList} />
+        <Route path="/characters" component={LocationList} />
         <Route component={WelComePage} />
       </Switch>
     </div>
