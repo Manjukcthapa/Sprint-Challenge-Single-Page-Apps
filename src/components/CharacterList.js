6c4ed1597945d579dcb42c534f368d9e3e77a7a1
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 11:20:48 -0400

    imported axios

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard"
+import axios from "axios"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [character, setCharacter] = useState({})
 
   useEffect(() => {
+    
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
