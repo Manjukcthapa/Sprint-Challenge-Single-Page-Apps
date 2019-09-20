cdf06f2ad08f257021ac92f19fddd55156645c78
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 11:15:12 -0400

    imported characterCard component

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(char) {
+  return (
+    <div>
+      <div>
+        <p>{char.name}</p>
+        <p>{char.status}</p>
+        <p>{char.species}</p>
+        <p>{char.type}</p>
+      </div>
+    </div>
+  );
 }
