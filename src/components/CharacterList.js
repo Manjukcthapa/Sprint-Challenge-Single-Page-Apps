96a6bfc778262a06893873d85d19f29be4cdd074
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 13:18:35 -0400

    added search functionality and its working

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,22 +2,19 @@ import React, { useEffect, useState } from "react";
 import CharacterCard from "./CharacterCard";
 import Styled from "styled-components";
 import axios from "axios";
-import SearchForm from "./SearchForm"
+import SearchForm from "./SearchForm";
 
 const Div = Styled.div`
 display: flex;
 flex-direction: row;
 flex-wrap: wrap;
-`
-
-
+`;
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
   const [characters, setCharacters] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
@@ -36,14 +33,24 @@ export default function CharacterList() {
 
   const handleChange = e => {
     e.preventDefault();
-  }
+    const searchTerm = e.target.value;
+    console.log(e.target.value);
+    setSearchTerm(searchTerm);
+    setSearchResults(
+      characters.filter(member => member.name.match(searchTerm))
+    );
+  };
 
   return (
     <Div>
       <SearchForm handleChange={handleChange} searchTerm={searchTerm} />
-      {characters.map(item => {
-        return <CharacterCard key={item.id} char={item} />;
-      })}
+      {searchResults.length
+        ? searchResults.map(item => {
+            return <CharacterCard key={item.id} char={item} />;
+          })
+        : characters.map(item => {
+            return <CharacterCard key={item.id} char={item} />;
+          })}
     </Div>
   );
 }
