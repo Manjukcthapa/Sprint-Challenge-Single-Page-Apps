ed5207fa1691f07f6e79f416cab142faeca0d93c
Manju KC <kcmanjuthapa@gmail.com>
2019-09-20 11:36:59 -0400

    feached the characters from API

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -4,17 +4,28 @@ import axios from "axios"
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
-  const [character, setCharacter] = useState({})
+  const [characters, setCharacters] = useState()
 
   useEffect(() => {
     
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+  axios.get (` https://rickandmortyapi.com/api/character/`)
+  .then(res => {
+    
+    setCharacters(res.data.results)
+    console.log('results', res.data.results)
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+     <CharacterCard/>
     </section>
   );
 }
